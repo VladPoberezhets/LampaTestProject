1acf10ab04369a8fd98ee3e03cf7f2627f40630a
Vladyslav Poberezhets <v.poberezhets.pnn.in.ua>
2024-10-28 17:33:12 +0200

    BUGFIX: fixed bug with decoding

--- LampaTestProject/DataModels/JSON Models/MovieModelsDTO.swift
+++ LampaTestProject/DataModels/JSON Models/MovieModelsDTO.swift
@@ -9,31 +9,27 @@ import Foundation
 
 // MARK: - Movies
 struct MovieModelsDTO: Codable {
-    let dates: Dates?
     let page: Int?
     let movies: [MovieDTO?]
     let totalPages: Int?
     let totalResults: Int?
 
     enum CodingKeys: String, CodingKey {
-        case dates, page
+        case page
         case movies = "results"
         case totalPages = "total_pages"
         case totalResults = "total_results"
     }
     
-    struct Dates: Codable {
-        let maximum, minimum: String
-    }
 }
 
 // MARK: - Result
 struct MovieDTO: Codable {
     let adult: Bool?
     let backdropPath: String?
-    let genreIDS: [Int]
+    let genreIDS: [Int]?
     let id: Int?
-    let originalLanguage: OriginalLanguage?
+    let originalLanguage: String?
     let originalTitle, overview: String?
     let popularity: Double?
     let posterPath, releaseDate, title: String?
@@ -54,12 +50,6 @@ struct MovieDTO: Codable {
         case title, video
         case voteAverage = "vote_average"
         case voteCount = "vote_count"
-    }
-    enum OriginalLanguage: String, Codable {
-        case en = "en"
-        case es = "es"
-        case ko = "ko"
-        case xx = "xx"
     }
 }
 
